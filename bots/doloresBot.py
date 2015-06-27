af7a5e1e6eeb4ffc38ae7742b9dcd0b6f39b5f19
Harsha <harsha@fieldsofview.in>
2015-06-28 01:20:45 +0530

    The new Bot with all functions.

--- bots/doloresBot.py
+++ bots/doloresBot.py
@@ -1,12 +1,12 @@
 __author__ = 'harsha'
 
 import telegram_methods.getMe
+import telegram_methods.getUpdates
 
 base_url = "https://api.telegram.org/bot"
 auth_token = "122152894:AAHV8afbjO_f4tPzmn-W8vLd7OWzWmxIbdw"
 
 my_bot = base_url + auth_token
 
-telegram_methods.getMe.getMe(my_bot)
-
+updates = telegram_methods.getUpdates.getUpdates(my_bot)
 
