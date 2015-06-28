66860adbb97398e55c484288f791cef0463a9b3c
harsha <harsha@fieldsofview.in>
2015-06-28 11:11:15 +0530

    Tesing a setup.py packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fovtelergrambot',
+    version='',
+    packages=['telegram', 'telegram_methods'],
+    url='',
+    license='',
+    author='bubby',
+    author_email='',
+    description=''
+)
