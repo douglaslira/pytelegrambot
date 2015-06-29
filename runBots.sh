9c898fc9b9a953463b8a9e075178adf8150cb949
Harsha <harsha@fieldsofview.in>
2015-06-29 11:49:06 +0530

    Added a logs folder for running bots

--- runBots.sh
+++ runBots.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Debian
-nohup /usr/bin/python3 bots/doloresBot.py >> doloresBot_log.txt &
+nohup /usr/bin/python3 bots/doloresBot.py >> logs/doloresBot_log.txt &
 
 #Arch
-#nohup /usr/bin/python bots/doloresBot.py >> doloresBot_log.txt &
+#nohup /usr/bin/python bots/doloresBot.py >> logs/doloresBot_log.txt &
 
