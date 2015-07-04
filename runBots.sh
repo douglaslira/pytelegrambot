60fef35e42a96634aed7eb277cadfea77ccdf9ba
harsha <harsha@fieldsofview.in>
2015-07-04 21:31:55 +0530

    redirecting logs to null

--- runBots.sh
+++ runBots.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#Debian With Logging
+#nohup /usr/bin/python3 bots/doloresBot.py >> logs/doloresBot.log &
+
 #Debian
-nohup /usr/bin/python3 bots/doloresBot.py >> logs/doloresBot.log &
+#nohup /usr/bin/python3 bots/doloresBot.py >> /dev/null &
 
 #Arch
 #nohup /usr/bin/python bots/doloresBot.py >> logs/doloresBot.log &
